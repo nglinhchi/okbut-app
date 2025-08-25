33b83ecff4dc6893958a7055b922d2f0a410302b
nglinhchi <linhchi311203@gmail.com>
2025-08-26 03:13:19 +1000

    chore: home page > basic layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <>home</>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className=" flex flex-col items-center justify-center gap-8 px-[20vw] py-[15vh] text-center w-full">
+        <h1>Ok, but...</h1>
+        <p className="lg:w-[40%]">
+          <strong>okbut.io</strong> lets you send fun, personalised cards to
+          your special ones — whether it's a “happy 100th birthday!!!”, “i
+          appreciate you”, “sorry i'll do better”, or just a “hey, this made me
+          think of you”.
+        </p>
+      </div>
+      <div className="background-gradient h-104 w-full"></div>
+    </div>
+  );
 }
