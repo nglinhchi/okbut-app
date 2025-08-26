dd22ba6bda7e1e9a45cc0e2c602be55a08ad027c
nglinhchi <linhchi311203@gmail.com>
2025-08-27 01:29:02 +1000

    chore: extract hardcoded data

---
+++ src/data/templates.ts
@@ -0,0 +1,28 @@
+import type { Template } from "../../types";
+
+export const TEMPLATES: Template[] = [
+  {
+    id: "1",
+    name: "Template 1",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+    image_url: "/src/assets/duck.jpg",
+    preview_card_id: "00000001",
+  },
+  {
+    id: "2",
+    name: "Template 2",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+    image_url: "/src/assets/gradient.webp",
+    preview_card_id: "00000002",
+  },
+  {
+    id: "3",
+    name: "Template 3",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
+    image_url: "/src/assets/confetti.jpg",
+    preview_card_id: "00000003",
+  },
+];
