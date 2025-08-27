3f77b24e6da366c574c7396c79dba825160f53c6
nglinhchi <linhchi311203@gmail.com>
2025-08-27 16:37:01 +1000

    chore: add TODOs

--- src/components/ShareLink.tsx
+++ src/components/ShareLink.tsx
@@ -1,6 +1,10 @@
 import Button from "./Button";
 
 export default function ShareLink() {
+  // TODO pull actual link from props
+  // TODO implement copy to clipboard
+  // TODO implement open link in new tab
+
   return (
     <div className="w-fit h-fit bg-white rounded-2xl p-8 text-black shadow-xl flex flex-col md:flex-row items-center justify-center gap-4">
       <div className="flex items-center justify-center px-8 py-4 border-2 border-black rounded-lg">
