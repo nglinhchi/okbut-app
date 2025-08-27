a22df5bd52af8bd90d0f8d741109f8eb72c84d8c
nglinhchi <linhchi311203@gmail.com>
2025-08-27 14:57:35 +1000

    feat: create component Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+const BUTTON_VARIANTS = {
+  default: "bg-black text-white",
+  muted: "bg-gray-200 text-black",
+  //   primary: "bg-primary text-black", // currently used for hover style
+  secondary: "bg-secondary text-black",
+};
+
+type ButtonVariant = keyof typeof BUTTON_VARIANTS;
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Button(props: ButtonProps) {
+  const { variant = "default", onClick, className, children, ...rest } = props;
+  const variantClassName = BUTTON_VARIANTS[variant] || BUTTON_VARIANTS.default;
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${variantClassName} px-12 py-2 rounded-xl w-fit h-fit transition ${className}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
