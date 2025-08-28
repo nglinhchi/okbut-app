78b853bac12dc1f244d06f3444e1c6457bc6c62f
nglinhchi <linhchi311203@gmail.com>
2025-08-28 18:27:25 +1000

    chore: reduce padding from template card

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -8,7 +8,7 @@ export default function Card(props: CardProps) {
 
   return (
     <div
-      className={`w-full bg-white rounded-2xl p-8 text-black shadow-xl flex flex-col gap-4 ${className}`}
+      className={`w-full bg-white rounded-2xl p-4 text-black shadow-xl flex flex-col gap-4 ${className}`}
     >
       {children}
     </div>
