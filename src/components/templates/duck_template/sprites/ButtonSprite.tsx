4abae4a55bdf32bd0f9d7a0330be4a0e087cb401
nglinhchi <linhchi311203@gmail.com>
2025-08-29 13:03:58 +1000

    chore: update ButtonSprite to use dynamic dimensions

--- src/components/templates/duck_template/sprites/ButtonSprite.tsx
+++ src/components/templates/duck_template/sprites/ButtonSprite.tsx
@@ -1,27 +1,26 @@
 import { useState } from "react";
 import spriteUrl from "../assets/button.png";
 
+const FRAME_WIDTH = 160;
+const FRAME_HEIGHT = 160;
+
 export default function ButtonSprite({ onClick }: { onClick: () => void }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
       onClick={onClick}
-      onMouseEnter={() => {
-        setIsHovered(true);
-      }}
-      onMouseLeave={() => {
-        setIsHovered(false);
-      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       className="cursor-pointer"
       style={{
-        width: "160px",
-        height: "160px",
+        width: `${FRAME_WIDTH}px`,
+        height: `${FRAME_HEIGHT}px`,
         backgroundImage: `url(${spriteUrl})`,
-        backgroundPosition: isHovered ? "-160px 0" : "0 0",
+        backgroundPosition: isHovered ? `-${FRAME_WIDTH}px 0` : "0 0",
         backgroundRepeat: "no-repeat",
-        backgroundSize: "320px 160px",
+        backgroundSize: `${FRAME_WIDTH * 2}px ${FRAME_HEIGHT}px`,
       }}
-    ></div>
+    />
   );
 }
