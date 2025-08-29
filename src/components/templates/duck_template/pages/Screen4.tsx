1d97fda7214668c7f0f8ddb37a8fdf687590bf48
nglinhchi <linhchi311203@gmail.com>
2025-08-29 13:06:34 +1000

    feat(duck): display card details in screen 4

--- src/components/templates/duck_template/pages/Screen4.tsx
+++ src/components/templates/duck_template/pages/Screen4.tsx
@@ -1,21 +1,78 @@
+import { useState } from "react";
 import type { CardData } from "../../../../../types";
+import TypeWriter from "typewriter-effect";
+import { cn } from "../../../../../lib/utils";
 
 interface Screen4Props {
   card: CardData;
 }
 
 export default function Screen4({ card }: Screen4Props) {
+  const [showMessage, setShowMessage] = useState(false);
+  const [showInscription, setShowInscription] = useState(false);
+
+  setTimeout(() => {
+    setShowMessage(true);
+    setTimeout(() => {
+      setShowInscription(true);
+    }, 3000);
+  }, 9000);
+
   return (
-    <div>
-      Duck Template:
-      <p>from: {card.sender}</p>
-      <p>to: {card.recipient}</p>
-      <p>message: {card.message}</p>
-      <img
-        src={`https://media.giphy.com/media/${card.giphy_id}/giphy.gif`}
-        alt="Selected GIF"
-        className="max-h-96 w-auto mx-auto"
-      />
+    <div className="flex flex-col md:flex-row w-full h-full bg-primary rounded-xl text-xl text-center md:text-left gap-8 p-6 md:p-8 overflow-hidden">
+      {/* IMAGE SECTION */}
+      <div className="flex items-center justify-center w-full md:basis-1/2 max-h-[80vh] overflow-hidden rounded-xl bg-black">
+        <img
+          src={`https://media.giphy.com/media/${card.giphy_id}/giphy.gif`}
+          alt="Selected GIF"
+          className="h-auto w-full max-h-[60vh] object-contain"
+        />
+      </div>
+
+      {/* TEXT SECTION */}
+      <div
+        className={cn(
+          "flex flex-col gap-4 w-full h-full justify-between md:justify-center items-center md:items-start md:gap-12 md:p-4 md:basis-1/2 overflow-y-auto",
+          card.message.length > 100
+            ? "text-sm md:text-lg"
+            : card.message.length > 50
+            ? "text-base md:text-xl"
+            : "text-lg md:text-2xl"
+        )}
+      >
+        <div className="flex flex-col gap-2">
+          <h1>
+            <TypeWriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("OK. ")
+                  .pauseFor(500)
+                  .typeString("THAT WAS FUN! ")
+                  .pauseFor(2000)
+                  .typeString("BUT...")
+                  .start();
+              }}
+            />
+          </h1>
+          <h1
+            className={cn(
+              "transition-opacity duration-1000",
+              showMessage ? "opacity-100" : "opacity-0"
+            )}
+          >
+            {card.message}
+          </h1>
+        </div>
+        <h1
+          id="inscription"
+          className={cn(
+            "transition-opacity duration-1000",
+            showInscription ? "opacity-100" : "opacity-0"
+          )}
+        >
+          - FROM {card.sender.toUpperCase()}, TO {card.recipient.toUpperCase()}
+        </h1>
+      </div>
     </div>
   );
 }
