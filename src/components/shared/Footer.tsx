de213c63349750c8f403e5e7afb7f25f0fed2aad
nglinhchi <linhchi311203@gmail.com>
2025-08-29 14:48:20 +1000

    chore: define fixed height for footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,13 +1,15 @@
 import Logo from "./Logo";
 
+// base: 20vh tall
+// sm and above: 10vh tall
+
 export default function Footer(props: { className?: string }) {
   const { className } = props;
   return (
-    // TODO set fixed height to calculate page height correctly
     <footer
-      className={`w-full px-12 py-12 sm:py-6 text-zinc-400 bg-black leading-none ${className}`}
+      className={`w-full h-[20vh] sm:h-[10vh] text-zinc-400 bg-black leading-none ${className} flex items-center justify-center`}
     >
-      <div className="flex flex-col items-center sm:flex-row sm:justify-between sm:items-end gap-2 ">
+      <div className="w-full h-full px-8 py-8 sm:py-4 flex flex-col items-center justify-center sm:flex-row sm:justify-between gap-2 ">
         <div className="flex flex-col items-center justify-center sm:flex-row sm:items-end gap-2">
           <Logo className="text-white" />
           <p>© {new Date().getFullYear()} All rights reserved.</p>
