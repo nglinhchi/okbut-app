7a27f97813925d5892f99566cc73211c04117e69
nglinhchi <linhchi311203@gmail.com>
2025-08-29 13:05:18 +1000

    chore(duck): update background colour of screen 1

--- src/components/templates/duck_template/pages/Screen1.tsx
+++ src/components/templates/duck_template/pages/Screen1.tsx
@@ -6,7 +6,7 @@ interface Screen1Props {
 
 export default function Screen1({ onStart }: Screen1Props) {
   return (
-    <div className="flex items-center justify-center h-full w-full bg-primary">
+    <div className="flex items-center justify-center h-full w-full bg-violet-400 rounded-xl">
       <ButtonSprite onClick={onStart} />
     </div>
   );
