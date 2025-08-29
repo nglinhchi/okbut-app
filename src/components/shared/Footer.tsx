babbfc5cf1880bfa5464c7468f90a0794155d7b7
nglinhchi <linhchi311203@gmail.com>
2025-08-29 13:02:31 +1000

    chore: edit layout of footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -5,20 +5,20 @@ export default function Footer(props: { className?: string }) {
   return (
     // TODO set fixed height to calculate page height correctly
     <footer
-      className={`w-full px-12 py-6 text-md text-zinc-400 bg-black ${className}`}
+      className={`w-full px-12 py-12 sm:py-6 text-zinc-400 bg-black leading-none ${className}`}
     >
-      <div className="flex flex-col md:flex-row md:justify-between md:items-end">
-        <div className="flex items-center justify-center gap-4">
+      <div className="flex flex-col items-center sm:flex-row sm:justify-between sm:items-end gap-2 ">
+        <div className="flex flex-col items-center justify-center sm:flex-row sm:items-end gap-2">
           <Logo className="text-white" />
-          <p>© {new Date().getFullYear()}All rights reserved.</p>
+          <p>© {new Date().getFullYear()} All rights reserved.</p>
         </div>
         <a
           href="https://chloecodes.io"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-zinc-200 transition"
         >
-          created by <strong>chloecodes.io</strong>
+          created by{" "}
+          <span className="text-white font-bold transition">chloecodes.io</span>
         </a>
       </div>
     </footer>
