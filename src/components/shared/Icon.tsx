cb19ecfa10883fcbf70860ceb41fe366aa6560d0
nglinhchi <linhchi311203@gmail.com>
2025-09-06 23:52:47 +1000

    chore: component icon, accept optional className

--- src/components/shared/Icon.tsx
+++ src/components/shared/Icon.tsx
@@ -3,9 +3,10 @@ import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 interface IconProps {
   icon: IconDefinition;
+  className?: string;
 }
 
 export default function Icon(props: IconProps) {
-  const { icon } = props;
-  return <FontAwesomeIcon icon={icon} />;
+  const { icon, className } = props;
+  return <FontAwesomeIcon icon={icon} className={className} />;
 }
