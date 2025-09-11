3508e4f5301b860d100414c3e5554686622e6fcc
nglinhchi <linhchi311203@gmail.com>
2025-09-11 15:34:50 +1000

    chore: tt, define line types

--- src/components/templates/terminal_template/TerminalTemplate.tsx
+++ src/components/templates/terminal_template/TerminalTemplate.tsx
@@ -1,4 +1,41 @@
 import type { TemplateProps } from "../../../pages/View";
+type LineCategory = "INFO" | "INPUT" | "BAR" | "GIF" | "BREAK";
+
+interface Line {
+  category: LineCategory;
+  duration: number;
+}
+
+interface BreakLine extends Line {
+  category: "BREAK";
+}
+
+interface InfoLine extends Line {
+  category: "INFO";
+  prompt: string;
+  stopNextLine?: boolean;
+  type?: "retry" | "exit" | "message" | "success";
+}
+
+interface InputLine extends Line {
+  category: "INPUT";
+  prompt: string;
+  attempts: number;
+  correctAnswer: (input: string) => boolean;
+  continueLine?: InfoLine;
+  exitLine: InfoLine;
+}
+
+interface BarLine extends Line {
+  category: "BAR";
+  onComplete: () => void;
+}
+
+// TODO might change to include both message + gif
+interface GifLine extends Line {
+  category: "GIF";
+  id: string;
+}
 
 export default function TerminalTemplate(props: TemplateProps) {
   const { card } = props;
